92114c4218787d53c9723a48d534975a90aa3b5c
StefanVinica <stefannikolovski996@gmail.com>
2022-12-10 23:12:23 +0100

    Personal Article Page Fetch

--- app/news/[articleId]/page.tsx
+++ app/news/[articleId]/page.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
+
 type PageProps = {
     params: {
-        articleId : string;
+        articleId: string;
     }
 }
 
-function ArticlePage({params:{articleId}}: PageProps){
-    //How to get the title
-    return(
-        <div >
-           Article ID: {articleId}
+
+const fetchArticle = async (articleId: string) => {
+    const body = {
+        query: {
+            match: {
+                id: articleId
+            }
+        }
+    };
+
+    const response = await fetch('http://167.172.142.105:5000/api/elasticsearch/leaves_articles/_search', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const news = await response.json();
+    return news;
+}
+
+async function ArticlePage({ params: { articleId } }: PageProps) {
+    const article = await fetchArticle(articleId)
+    return (
+        <div className="p-4 border-2 rounded-lg bg-white">
+          <h2 className="text-xl font-bold">Article ID: {articleId}</h2>
+          <p className="text-gray-700">
+            Article Title: {article.hits.hits[0]._source.title}
+          </p>
         </div>
-    )
+      )
 
 }
 
--- typings.d.tsx
+++ typings.d.tsx
@@ -2,3 +2,15 @@ export type Leaf = {
     id: string;
     title: string;
 }
+
+export type Article = {
+    hits: {
+      hits: [
+        {
+          _source: {
+            title: string;
+          };
+        }
+      ];
+    };
+  };
