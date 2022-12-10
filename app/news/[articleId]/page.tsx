8ecde65f6601681a2e961ce73056b1b7e8f44f20
StefanVinica <stefannikolovski996@gmail.com>
2022-12-10 23:32:44 +0100

    Cache

--- app/news/[articleId]/page.tsx
+++ app/news/[articleId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Leaf } from '../../../typings.d';
 
 type PageProps = {
     params: {
@@ -22,6 +23,7 @@ const fetchArticle = async (articleId: string) => {
         headers: {
             'Content-Type': 'application/json',
         },
+        next: { revalidate: 60 },
     });
     const news = await response.json();
     return news;
@@ -31,13 +33,32 @@ async function ArticlePage({ params: { articleId } }: PageProps) {
     const article = await fetchArticle(articleId)
     return (
         <div className="p-4 border-2 rounded-lg bg-white">
-          <h2 className="text-xl font-bold">Article ID: {articleId}</h2>
-          <p className="text-gray-700">
-            Article Title: {article.hits.hits[0]._source.title}
-          </p>
+            <h2 className="text-xl font-bold">Article ID: {articleId}</h2>
+            <p className="text-gray-700">
+                Article Title: {article.hits.hits[0]._source.title}
+            </p>
         </div>
-      )
+    )
 
 }
 
 export default ArticlePage
+
+export async function generateStaticParams() {
+    const fetchNews = async (): Promise<Array<Leaf> | undefined> => {
+        try {
+            const response = await fetch('http://167.172.142.105:5000/cassandra-leaves');
+            const json = await response.json();
+            const news = json as Array<Leaf>;
+            return news.map(article => ({
+                id: article.id,
+                // Add other required properties here
+                title: article.title,
+                // ...
+            }));
+        } catch (err) {
+            console.error(err);
+            return undefined;
+        }
+    };
+}
